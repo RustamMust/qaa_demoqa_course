4f4d5ca2916b69b7363d523b460530ba5a7a89df
RustamMust <rustammustavaev@gmail.com>
2024-07-30 18:53:28 +0500

    added tests: - test_check_link - test_broken_link

--- tests/test_elements_page.py
+++ tests/test_elements_page.py
@@ -1,6 +1,6 @@
 import random
 import time
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, LinksPage
 
 
 class TestElements:
@@ -94,20 +94,15 @@ class TestElements:
             assert right == 'You have done a right click', 'Right button has not been clicked'
             assert click == 'You have done a dynamic click', 'Click button has not been clicked'
 
+    class TestLinksPage:
+        def test_check_link(self, driver):
+            links_page = LinksPage(driver, 'https://demoqa.com/links')
+            links_page.open()
+            href_link, current_url = links_page.check_new_tab_simple_link()
+            assert href_link == current_url, 'Link is broken or URL is incorrect'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        def test_broken_link(self, driver):
+            links_page = LinksPage(driver, 'https://demoqa.com/links')
+            links_page.open()
+            response_code = links_page.check_broken_link('https://demoqa.com/bad-request')
+            assert response_code == 400, 'Link works or the status code is not 400'
