968cce1017f75d9f507cfd3de80eb47f68a91334
RustamMust <rustammustavaev@gmail.com>
2024-07-31 10:47:38 +0500

    added tests: - test_upload_file - test_download_file - test_dynamic_properties - test_appear_of_button - test_enable_of_button

--- generator/generator.py
+++ generator/generator.py
@@ -21,3 +21,9 @@ def generated_person():
     )
 
 
+def generated_file():
+    path = f'/Users/qamacos/PycharmProjects/qaa_demoqa_course/filetest{random.randint(0,999)}.txt'
+    file = open(path, 'w+')
+    file.write(f'Hello World{random.randint(0, 999)}')
+    file.close()
+    return file.name, path
--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -68,3 +68,15 @@ class ButtonsPageLocators:
 class LinksPageLocators:
     SIMPLE_LINK = (By.CSS_SELECTOR, "a[id='simpleLink']")
     BAD_REQUEST = (By.CSS_SELECTOR, "a[id='bad-request']")
+
+
+class UploadAndDownloadPageLocators:
+    UPLOAD_FILE = (By.CSS_SELECTOR, "input[id='uploadFile']")
+    UPLOADED_FILE = (By.CSS_SELECTOR, "p[id='uploadedFilePath']")
+    DOWNLOAD_FILE = (By.CSS_SELECTOR, "a[id='downloadButton']")
+
+
+class DynamicPropertiesPageLocators:
+    COLOR_CHANGE_BUTTON = (By.CSS_SELECTOR, "button[id='colorChange']")
+    VISIBLE_AFTER_FIVE_SEC_BUTTON = (By.CSS_SELECTOR, "button[id='visibleAfter']")
+    ENABLE_AFTER_FIVE_SEC_BUTTON = (By.CSS_SELECTOR, "button[id='enableAfter']")
